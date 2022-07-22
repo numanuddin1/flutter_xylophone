19ee176bd3457e9de9349abfc4fbd57d367b4de6
Numan <nomanuddin2012@gmail.com>
2022-07-22 08:56:06 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final sound = AssetSource('note$soundNumber.wav');
+    AudioPlayer().play(sound);
+  }
+
+  Widget buildKey(int soundNumber, Color color) {
+
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          playSound(soundNumber);
+        },
+        child: Container(
+          height: 40,
+          width: 120,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
